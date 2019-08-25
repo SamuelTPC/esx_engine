787503f898cbf3fce06be112f9ecc82f9fe76b65
SamuelGamerPT <47862397+SamuelGamerPT@users.noreply.github.com>
2019-08-25 11:25:51 +0100

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -14,7 +14,7 @@ end)
             local engineStatus = GetIsVehicleEngineRunning(vehicle)
             local item = inventory.items
             if item.count >= 1 then
-                exports['progressBars']:startUI(15000, "A cortar os fios do carro...")
+                exports['progressBars']:startUI(15000, "Cutting the car cables...")
                 Citizen.Wait(15000)
                 SetVehicleEngineOn(vehicle, false, true, true)
                 SetVehicleEngineHealth(vehicle, 0)
